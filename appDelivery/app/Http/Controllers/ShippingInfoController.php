813d284e5fdfb1462c81efcc2fa768d045069671
tomficher <fichtner.dk@hotmail.com>
2023-01-16 16:13:15 -0300

    create mcr Customer,Order and Shipping

---
+++ appDelivery/app/Http/Controllers/ShippingInfoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreShippingInfoRequest;
+use App\Http\Requests\UpdateShippingInfoRequest;
+use App\Models\ShippingInfo;
+
+class ShippingInfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreShippingInfoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreShippingInfoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ShippingInfo  $shippingInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ShippingInfo $shippingInfo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ShippingInfo  $shippingInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ShippingInfo $shippingInfo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateShippingInfoRequest  $request
+     * @param  \App\Models\ShippingInfo  $shippingInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateShippingInfoRequest $request, ShippingInfo $shippingInfo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ShippingInfo  $shippingInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ShippingInfo $shippingInfo)
+    {
+        //
+    }
+}
