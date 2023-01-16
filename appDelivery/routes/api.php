d5203d3645459b82d2a17088b1152a5aafcd5076
tomficher <fichtner.dk@hotmail.com>
2023-01-16 18:04:40 -0300

    mapping routes

--- appDelivery/routes/api.php
+++ appDelivery/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('customer','App\Http\Controllers\CustomerController');
+
+Route::apiResource('order','App\Http\Controllers\OrderController');
+
+Route::apiResource('shipping-info','App\Http\Controllers\ShippingInfoController');
