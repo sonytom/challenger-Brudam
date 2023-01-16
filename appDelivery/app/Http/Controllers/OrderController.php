127907e74571b5b179d126bb1d4a11df5961b6a9
tomficher <fichtner.dk@hotmail.com>
2023-01-16 18:05:10 -0300

    deleted methods unused

--- appDelivery/app/Http/Controllers/OrderController.php
+++ appDelivery/app/Http/Controllers/OrderController.php
@@ -19,16 +19,6 @@ class OrderController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreOrderRequest  $request
@@ -46,17 +36,6 @@ class OrderController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Order $order)
     {
         //
     }
--- appDelivery/app/Http/Controllers/ShippingInfoController.php
+++ appDelivery/app/Http/Controllers/ShippingInfoController.php
@@ -19,16 +19,6 @@ class ShippingInfoController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreShippingInfoRequest  $request
@@ -46,17 +36,6 @@ class ShippingInfoController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(ShippingInfo $shippingInfo)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\ShippingInfo  $shippingInfo
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(ShippingInfo $shippingInfo)
     {
         //
     }
