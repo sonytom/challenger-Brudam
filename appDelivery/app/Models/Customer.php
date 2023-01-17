0881e00f8b9ae4d653182997c2c8a28abc93d720
tomficher <fichtner.dk@hotmail.com>
2023-01-17 20:52:59 -0300

    add rules for customer and msg for feedback

--- appDelivery/app/Models/Customer.php
+++ appDelivery/app/Models/Customer.php
@@ -8,5 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','image','address'];
+    protected $fillable = ['name', 'image', 'address'];
+
+    public function rules($id)
+    {
+        $id = $id === null ? 'null' : $id;
+
+
+        return [
+            'name' => "required|unique:customers,name," . $id . "|min:3",
+            'image' => 'required|file|mimes:png,pdf',
+            'address' => 'required'
+        ];
+    }
+
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O field :attribute is required',
+            'image.mimes' => 'Not Suported type,is alowed : png,pdf ',
+            'name.unique' => 'Name already exists',
+            'name.min' => 'minimum three characters'
+        ];
+    }
 }
