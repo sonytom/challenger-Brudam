54254e63bd5a7aac212ea8ff5ead2ad02f3e13e8
tomficher <fichtner.dk@hotmail.com>
2023-01-18 10:21:02 -0300

    implement all methods for order

--- appDelivery/app/Http/Controllers/OrderController.php
+++ appDelivery/app/Http/Controllers/OrderController.php
@@ -8,6 +8,15 @@ use App\Models\Order;
 
 class OrderController extends Controller
 {
+
+    protected $order;
+    // TypeHitting Objeto
+    //injeção
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,18 +24,28 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $orders = $this->order->all();
+        return $orders;
     }
 
     /**
      * Store a newly created resource in storage.
-     *
+     * 
      * @param  \App\Http\Requests\StoreOrderRequest  $request
      * @return \Illuminate\Http\Response
      */
     public function store(StoreOrderRequest $request)
     {
-        //
+        $request->validate($this->order->rules(null));
+
+        $order = $this->order->create([
+            'customers_id' => $request->customers_id,
+            'dateDelivery' => $request->dateDelivery,
+            'taxSend' => $request->taxSend,
+            'fragile' => $request->fragile
+        ]);
+
+        return response()->json($order, 201);
     }
 
     /**
@@ -35,9 +54,13 @@ class OrderController extends Controller
      * @param  \App\Models\Order  $order
      * @return \Illuminate\Http\Response
      */
-    public function show(Order $order)
+    public function show($id)
     {
-        //
+        $order = $this->order->find($id);
+        if ($order === null) {
+            return response()->json(['erro' => 'Não existe'], 404);
+        }
+        return response()->json($order, 200);
     }
 
     /**
@@ -47,9 +70,37 @@ class OrderController extends Controller
      * @param  \App\Models\Order  $order
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateOrderRequest $request, Order $order)
+    public function update(UpdateOrderRequest $request, $id)
     {
-        //
+        $order = $this->order->find($id);
+
+        if ($order === null) {
+            return response()->json(['erro' => 'Não existe'], 404);
+        }
+
+        if (request()->method() === 'PATCH') {
+            $rulesDynamics = array();
+
+            foreach ($order->rules($id) as $input => $rule) {
+
+                if (array_key_exists($input, $request->all())) {
+                    $rulesDynamics[$input] = $rule;
+                }
+            }
+            $request->validate($rulesDynamics);
+        } else {
+            $request->validate($this->order->rules($id));
+        }
+
+
+        $order->update([
+            'customers_id' => $request->customers_id,
+            'dateDelivery' => $request->dateDelivery,
+            'taxSend' => $request->taxSend,
+            'fragile' => $request->fragile
+        ]);
+
+        return response()->json($order, 200);
     }
 
     /**
@@ -58,8 +109,14 @@ class OrderController extends Controller
      * @param  \App\Models\Order  $order
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Order $order)
+    public function destroy($id)
     {
-        //
+        $order = $this->order->find($id);
+        if ($order === null) {
+            return response()->json(['erro' => 'Não existe'], 404);
+        }
+
+        $order->delete();
+        return response()->json(['msg' => 'successfully deleted record'], 200);
     }
 }
