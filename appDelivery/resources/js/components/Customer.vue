abd9c0b8be11787341166c40cb241804de022385
tomficher <fichtner.dk@hotmail.com>
2023-01-18 23:32:58 -0300

    started dev frontend
    s

---
+++ appDelivery/resources/js/components/Customer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"> Bem Vind</div>
+    
+                    <div class="card-body">
+                   Indicadores do sistema
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </template>
+    
+    <script>
+    
+    </script>
